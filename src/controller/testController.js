226f35d528d5c8a2a3cfada6766a87028b2934a9
Juan Salcedo <juan.salcedo@mlb.com>
2021-07-20 14:32:32 -0600

    Storing the ChannelUri

--- src/controller/testController.js
+++ src/controller/testController.js
@@ -1,11 +1,20 @@
 
+const appConfig = require('../models/appConfig');
+
 export function roboxTest(request, response) {
     response.json({ name: `Juan`, age: './15' });
+    console.log(`channelUri: ${appConfig.channelUri}`);
 };
 
 export function notification(request, response){
     let body =  request.body;
+    console.log(`body.channelUri: ${body.channelUri}`);
+    if (body.channelUri){
+        if (appConfig.channelUri !==  body.channelUri) {
+            appConfig.channelUri =  body.channelUri
+        }
+    }
     return response.json({
-        name: body.name
+        channelUri: appConfig.channelUri
     });
 }
---
+++ src/models/appConfig.js
@@ -0,0 +1,7 @@
+class appConfig {
+    constructor() {
+        this.channelUri = '';
+    }
+}
+
+module.exports = new appConfig();
