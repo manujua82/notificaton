2358deca22f04b409d3348585e67a75bce9748fa
Juan Salcedo <juan.salcedo@mlb.com>
2021-07-20 10:53:10 -0600

    Updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import { routes } from './routes/routes';
 import path from 'path';
 import cors from 'cors';
 
-// const result = dotenv.config({path: __dirname + '/.env'});
-// if (result.error) {
-//     console.log(result.error)
-// }
 
 const app = express();
 const port = process.env.PORT || 3000;
