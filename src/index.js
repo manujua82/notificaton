fcd20a3f09da22e091aa77ff798c0014fb62f53b
Nicholas Wheeler <nicholas.wheeler@mlb.com>
2021-07-21 10:54:07 -0600

    Fixed config problem

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const dotenv = require('dotenv');
 const appConfig = require('./models/appConfig');
 
 dotenv.config();
-const serviceAccount = require(process.env.GOOGLE_FIRESTORE);
 const app = express();
+const serviceAccount = JSON.parse(process.env.GOOGLE_FIRESTORE);
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
@@ -23,7 +23,7 @@ routes(app);
 // app.set("port", port)
 app.listen( port, '0.0.0.0', () => {
     console.log(`your server is running on port ${port}`);
-    initializeFirebase();
+    // initializeFirebase();
 });
 
 
